d83bc1e984b23fe4c3389865669a8779f174988b
kmankan <malink027@gmail.com>
2024-03-25 23:45:33 +1100

    modify repeatStrings to pass all test criteria

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repetition = ''
+  if (num >= 0) {
+    for (i = 0; i < num; i++) {
+      repetition += string;
+    }
+    return repetition
+  } else return "ERROR"
 };
 
 // Do not edit below this line
