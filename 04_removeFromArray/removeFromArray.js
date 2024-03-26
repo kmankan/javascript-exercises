45d2627695a96fabc71a8e4733bb2bdb1de22878
kmankan <malink027@gmail.com>
2024-03-27 10:39:21 +1100

    Create general function that meets all test cases for removing specific items from array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...args) {
+  let newArray = originalArray;
+  for (i = 0; i < arguments.length; i++) {
+    newArray = newArray.filter(num => num !== args[i]); //if the element is true it will be included in the new Array
+  }
+  return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/* Key learnings:
+> ...args is a rest parameter that collects all subsequent arguments
+> filter(): array.filter(x => x.comparison);
+  x.comparison is the condition that each element x is tested against. 
+  It could be any valid expression that evaluates to a boolean value (true or false). 
+  For example, x.property === value or x > 10.
+ */
