8985403711f6cf5ac7ffa97aa1832f95811dee99
kmankan <malink027@gmail.com>
2024-04-03 02:19:05 +1100

    build different function for a calculator app that passess all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
+  }
+
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+	const sumArray = arr.reduce((pv, cv) => pv + cv, 0);
+    return +sumArray;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let multiplyResult = 1;
+  for (let i=0; i < arr.length; i++) {
+    multiplyResult *= arr[i];
+  }
+  return multiplyResult;
 };
 
-const multiply = function() {
-
+const power = function(base, exponent) {
+	const result = base ** exponent;
+  return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      fact *= i
+    }
+    return fact; 
+  } 
+  else return 1;  
 };
 
 // Do not edit below this line
@@ -31,3 +44,4 @@ module.exports = {
   power,
   factorial
 };
+
